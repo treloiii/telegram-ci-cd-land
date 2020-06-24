af3d0d78dbcd335d4b13cf354dee2f0d0197e39d
trelloiii <nikkraew22@gmail.com>
2020-06-24 14:01:40 +0300

    navbar and font

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar></navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from "@/components/Navbar";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Navbar},
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @font-face {
+    font-family: 'JetBrains Mono Regular';
+    src: url("~@/assets/18893.ttf");
+  }
+  body{
+    font-family: 'JetBrains Mono Regular',serif;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card
+    color="grey lighten-4"
+    flat
+    height="300px"
+    tile
+    >
+    <v-toolbar dense>
+        <v-toolbar-title>Telegram CI/CD bot</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-btn text>Download</v-btn>
+        <v-btn text>Getting started</v-btn>
+        <v-btn text>Github</v-btn>
+    </v-toolbar>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
